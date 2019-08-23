4577b0c9f33ed51af8b00210f080a138ac807a6e
thealexandar <admitrovic@yahoo.com>
2019-08-23 14:37:35 +0200

    alfa

--- js/main.js
+++ js/main.js
@@ -1,8 +1,64 @@
 // $(document).ready(function() {
 //     setTimeout(function(){
 //         $('#body').show();
-//         $('#msg').hide();
-//     }, 2000);
+//         $('#main').show();
+//         $('#loader').hide();
+//     }, 1700);
 
 // });
 
+
+// Preloader
+$('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+
+    {
+
+      duration: 1000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        $('.counter').addClass("fadeOut");
+      }
+
+    });
+  });
+
+  (function($) {
+    $.fn.visible = function(partial) {
+
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+  })(jQuery);
+
+  $(window).scroll(function(event) {
+
+    $("p").each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("fadeIn");
+      } else {
+        el.removeClass("fadeIn");
+      }
+    });
+
+  });
--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -4,12 +4,27 @@ sr.reveal('.fadeInCenter', {
     origin: 'center',
     distance: '150px',
     opacity: 0
-  });
+});
+sr.reveal('.fadeInFromLeft', {
+    duration: 1500,
+    origin: 'left',
+    distance: '150px',
+    opacity: 0,
+    viewFactor: 0.1
+});
+sr.reveal('.fadeInFromRight', {
+    duration: 1500,
+    origin: 'right',
+    distance: '150px',
+    opacity: 0,
+    viewFactor: 0.1
+});
 sr.reveal('.fadeInTop', {
-  duration: 2500,
-  origin: 'top',
-  distance: '350px',
-  opacity: 0
+  duration: 1500,
+  origin: 'bottom',
+  distance: '50px',
+  opacity: 0,
+  viewFactor: 0.2
 });
 sr.reveal('.fadeInBottom', {
     duration: 1500,
@@ -44,4 +59,12 @@ sr.reveal('.fadeInBottomSlow', {
     distance: '350px',
     opacity: 0,
     easing: 'ease'
+});
+sr.reveal('.slideOutFromRight', {
+    duration: 1000,
+    origin: 'right',
+    distance: '100%',
+    easing: 'cubic-bezier(0.66, 0.01, 0.19, 1)',
+    opacity: 1,
+    viewFactor: 0.2
 });
